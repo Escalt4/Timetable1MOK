aca22cc44e2d8932c389db704ad802d07863451f
Escalt4 <escalt4@gmail.com>
2022-10-07 00:07:25 +0300

    Updates and fixes

--- app/src/main/java/com/example/timetable1mok/MainActivity.java
+++ app/src/main/java/com/example/timetable1mok/MainActivity.java
@@ -50,11 +50,12 @@ public class MainActivity extends AppCompatActivity {
     ViewPager2 pager;
 
     String[][][][][] timetable;
+    String[][][][] basicTimetable;
     String[][] calls;
     MyTime[][] callsTime = new MyTime[2][11];
 
-    MyTime[][] baseCallMonday;
-    MyTime[][] baseCallOther;
+    MyTime[][] basicCallsScheduleMonday;
+    MyTime[][] basicCallsScheduleOther;
 
     MyTime[][][][][] callSchedule;
 
@@ -184,22 +185,22 @@ public class MainActivity extends AppCompatActivity {
             byte[] buffer = new byte[inputStream.available()];
             inputStream.read(buffer);
             inputStream.close();
-            String text = new String(buffer);
+            String text = new String(buffer).replace("\r", "");
 
             String[] callsBlocks = text.split("========");
 
             String[] Monday = callsBlocks[0].split("\n");
             String[] Other = callsBlocks[1].split("\n");
 
-            // номер пары; время начала или конца
-            baseCallMonday = new MyTime[5][2];
-            baseCallOther = new MyTime[5][2];
+            // номер пары; время начала или конца;
+            basicCallsScheduleMonday = new MyTime[5][2];
+            basicCallsScheduleOther = new MyTime[4][2];
 
             for (int i = 0; i < Monday.length; i++) {
-                baseCallMonday[i] = splitCallsString(Monday[i]);
+                basicCallsScheduleMonday[i] = splitCallsString(Monday[i]);
             }
             for (int i = 0; i < Other.length; i++) {
-                baseCallOther[i] = splitCallsString(Other[i]);
+                basicCallsScheduleOther[i] = splitCallsString(Other[i]);
             }
 
         } catch (Exception ex) {
@@ -216,233 +217,244 @@ public class MainActivity extends AppCompatActivity {
             byte[] buffer = new byte[inputStream.available()];
             inputStream.read(buffer);
             inputStream.close();
-            String text = new String(buffer);
+            String text = new String(buffer).replace("\r", "");
 
             String[] timetableBlocks = text.split("========");
 
-            //
-            timetable = new String[5][2][2][5][4];
+            // номер дня недели; группа; тип недели (верхняя / нижняя); какая пара по счету
+            basicTimetable = new String[5][2][2][5];
+
 
             for (int i = 0; i < timetableBlocks.length; i++) {
-                String[] blocksPart = timetableBlocks[i].split("--------");
+                String[] string = timetableBlocks[i].split("\n");
+                for (int j = 1; j < string.length; j = j + 6) {
+                    string[j].split(" ");
+                }
+                    basicTimetable[i][][][]
 
-                if (i == 0) {
-                    for (int j = 1; j < blocksPart.length; j++) {
-                        String[] strings = blocksPart[j].split("\n");
+                }
 
-                        if (j == 1) {
-                            for (int k = 1; k < strings.length; k++) {
-//                                callSchedule[0][k - 1] = splitCallsString(strings[k]);
-                                calls[0][k - 1] = strings[k].replace("\"", "").replace("\r", "").replace("\n", "");
-                            }
-                        } else {
-                            for (int k = 1; k < strings.length; k++) {
-//                                callSchedule[1][k - 1] = splitCallsString(strings[k]);
-                                calls[1][k - 1] = strings[k].replace("\"", "").replace("\r", "").replace("\n", "");
-                            }
-                        }
-                    }
 
-                } else {
-                    for (int j = 1; j < blocksPart.length; j++) {
-                        String[] strings = blocksPart[j].split("\n");
+//                for (int i = 0; i < timetableBlocks.length; i++) {
+//                    String[] blocksPart = timetableBlocks[i].split("--------");
+//
+//                    if (i == 0) {
+//                        for (int j = 1; j < blocksPart.length; j++) {
+//                            String[] strings = blocksPart[j].split("\n");
+//
+//                            if (j == 1) {
+//                                for (int k = 1; k < strings.length; k++) {
+////                                callSchedule[0][k - 1] = splitCallsString(strings[k]);
+//                                    calls[0][k - 1] = strings[k].replace("\"", "").replace("\r", "").replace("\n", "");
+//                                }
+//                            } else {
+//                                for (int k = 1; k < strings.length; k++) {
+////                                callSchedule[1][k - 1] = splitCallsString(strings[k]);
+//                                    calls[1][k - 1] = strings[k].replace("\"", "").replace("\r", "").replace("\n", "");
+//                                }
+//                            }
+//                        }
+//
+//                    } else {
+//                        for (int j = 1; j < blocksPart.length; j++) {
+//                            String[] strings = blocksPart[j].split("\n");
+//
+//                            for (int k = 1; k < strings.length; k = k + 6) {
+//                                String[] temp1 = strings[k].split(" ");
+//
+//                                for (int q = 0; q < temp1.length; q++) {
+//                                    int groop_ = Integer.parseInt(Character.toString(temp1[q].charAt(0))) - 1;
+//                                    int week_ = Integer.parseInt(Character.toString(temp1[q].charAt(1))) - 1;
+//                                    int day_ = Integer.parseInt(strings[k + 1].replace("\r", "")) - 1;
+//                                    Log.d(LogTag, day_ + " ");
+//                                    for (int w = 0; w < 4; w++) {
+//                                        callSchedule[i - 1][groop_][week_][day_] = splitCallsString(calls[week_][i - 1]);
+//                                        timetable[i - 1][groop_][week_][day_][w] = strings[k + 2 + w].replace("\r", "").replace("\n", "");
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+                Log.d(LogTag, MyTime.timesFormatString(callSchedule[0][1][1][1][0]));
+                Log.d(LogTag, MyTime.timesFormatString(callSchedule[0][1][1][1][1]));
 
-                        for (int k = 1; k < strings.length; k = k + 6) {
-                            String[] temp1 = strings[k].split(" ");
+                setTimetable();
 
-                            for (int q = 0; q < temp1.length; q++) {
-                                int groop_ = Integer.parseInt(Character.toString(temp1[q].charAt(0))) - 1;
-                                int week_ = Integer.parseInt(Character.toString(temp1[q].charAt(1))) - 1;
-                                int day_ = Integer.parseInt(strings[k + 1].replace("\r", "")) - 1;
-                                Log.d(LogTag, day_ + " ");
-                                for (int w = 0; w < 4; w++) {
-                                    callSchedule[i - 1][groop_][week_][day_] = splitCallsString(calls[week_][i - 1]);
-                                    timetable[i - 1][groop_][week_][day_][w] = strings[k + 2 + w].replace("\r", "").replace("\n", "");
-                                }
-                            }
-                        }
-                    }
+                initializeCalls();
+                startNewTimer();
+            } catch(Exception ex){
+                Log.e(LogTag, Log.getStackTraceString(ex));
+            }
+        }
+
+
+        void setTimetable () {
+            pager = findViewById(R.id.pager);
+            FragmentStateAdapter pageAdapter = new MyAdapter(this, timetable, calls, group, week);
+            pager.setAdapter(pageAdapter);
+
+            TabLayout tabLayout = findViewById(R.id.tab_layout);
+            TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, pager, new TabLayoutMediator.TabConfigurationStrategy() {
+                @Override
+                public void onConfigureTab(TabLayout.Tab tab, int position) {
+                    tab.setText(tabsName[position] + "\n" + weekDays[position] + " " + monthName[weekMonth[position]]);
+                }
+            });
+            tabLayoutMediator.attach();
+
+            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    buttonToCurrentDate.setEnabled(true);
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+                }
+            });
+
+            pager.setCurrentItem(currentTab, false);
+            buttonToCurrentDate.setEnabled(false);
+
+        }
+
+
+        void initializeCalls () {
+            for (int i = 0; i < 2; i++) {
+                int k = 0;
+                for (int j = 0; j < (5 - i); j++) {
+                    callsTime[i][k] = new MyTime(
+                            Integer.parseInt(calls[i][j].split("-")[0].split("\\.")[0]),
+                            Integer.parseInt(calls[i][j].split("-")[0].split("\\.")[1])
+                    );
+
+                    callsTime[i][k + 1] = new MyTime(
+                            Integer.parseInt(calls[i][j].split("-")[1].split("\\.")[0]),
+                            Integer.parseInt(calls[i][j].split("-")[1].split("\\.")[1])
+                    );
+                    k += 2;
                 }
             }
-            Log.d(LogTag, MyTime.timesFormatString(callSchedule[0][1][1][1][0]));
-            Log.d(LogTag, MyTime.timesFormatString(callSchedule[0][1][1][1][1]));
-
-            setTimetable();
-
-            initializeCalls();
-            startNewTimer();
-        } catch (Exception ex) {
-            Log.e(LogTag, Log.getStackTraceString(ex));
-        }
-    }
-
-
-    void setTimetable() {
-        pager = findViewById(R.id.pager);
-        FragmentStateAdapter pageAdapter = new MyAdapter(this, timetable, calls, group, week);
-        pager.setAdapter(pageAdapter);
-
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, pager, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(TabLayout.Tab tab, int position) {
-                tab.setText(tabsName[position] + "\n" + weekDays[position] + " " + monthName[weekMonth[position]]);
-            }
-        });
-        tabLayoutMediator.attach();
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                buttonToCurrentDate.setEnabled(true);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-
-        pager.setCurrentItem(currentTab, false);
-        buttonToCurrentDate.setEnabled(false);
-
-    }
-
-
-    void initializeCalls() {
-        for (int i = 0; i < 2; i++) {
-            int k = 0;
-            for (int j = 0; j < (5 - i); j++) {
-                callsTime[i][k] = new MyTime(
-                        Integer.parseInt(calls[i][j].split("-")[0].split("\\.")[0]),
-                        Integer.parseInt(calls[i][j].split("-")[0].split("\\.")[1])
-                );
-
-                callsTime[i][k + 1] = new MyTime(
-                        Integer.parseInt(calls[i][j].split("-")[1].split("\\.")[0]),
-                        Integer.parseInt(calls[i][j].split("-")[1].split("\\.")[1])
-                );
-                k += 2;
-            }
-        }
 //        callsTime[0][10] = MyTime.additionTimes(callsTime[0][0], new MyTime(24, 0, 0));
 //        callsTime[1][8] = MyTime.additionTimes(callsTime[1][0], new MyTime(24, 0, 0));
-    }
+        }
 
 
-    String label;
-    Integer curPair = 0;
-    ;
-    MyTime cur;
-    MyTime timeStart;
-    MyTime timeEnd;
-    Handler handler = new Handler();
+        String label;
+        Integer curPair = 0;
+        ;
+        MyTime cur;
+        MyTime timeStart;
+        MyTime timeEnd;
+        Handler handler = new Handler();
 
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                cur = new MyTime(
+                        Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                        Calendar.getInstance().get(Calendar.MINUTE),
+                        Calendar.getInstance().get(Calendar.SECOND)
+                );
+
+                if (MyTime.isBetweenTimes(cur, timeStart, timeEnd)) {
+                    textViewTimer.setText(label + MyTime.subtractionTimesFormatString(timeEnd, cur));
+                    EventBus.getDefault().post(new MessageEvent(true, curPair, currentDayNum));
+                    doCicleTimer();
+                } else {
+                    if (curPair != 0) {
+                        EventBus.getDefault().post(new MessageEvent(false, curPair, currentDayNum));
+                    }
+                    startNewTimer();
+                }
+            }
+        };
+
+
+        // таймер до начала/конца пары
+        public void doCicleTimer () {
+            handler.postDelayed(runnable, 250);
+        }
+
+
+        public void startNewTimer () {
+            if (currentDayNum == 5 || currentDayNum == 6) {
+                textViewTimer.setText("Сейчас нет пар");
+                return;
+            }
+
             cur = new MyTime(
                     Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
                     Calendar.getInstance().get(Calendar.MINUTE),
                     Calendar.getInstance().get(Calendar.SECOND)
             );
 
-            if (MyTime.isBetweenTimes(cur, timeStart, timeEnd)) {
-                textViewTimer.setText(label + MyTime.subtractionTimesFormatString(timeEnd, cur));
-                EventBus.getDefault().post(new MessageEvent(true, curPair, currentDayNum));
-                doCicleTimer();
-            } else {
-                if (curPair != 0) {
-                    EventBus.getDefault().post(new MessageEvent(false, curPair, currentDayNum));
+            boolean started = false;
+
+            for (int i = 0; i < (10 - calls_type * 2) - 1; i++) {
+                if (MyTime.isBetweenTimes(cur, callsTime[calls_type][i], callsTime[calls_type][i + 1])) {
+                    if (i % 2 == 0) {
+                        label = "До конца пары:\n";
+                        curPair = i / 2 + 1;
+                    } else {
+                        curPair = 0;
+                        label = "До начала пары:\n";
+                    }
+                    timeStart = callsTime[calls_type][i];
+                    timeEnd = callsTime[calls_type][i + 1];
+                    started = true;
+                    doCicleTimer();
+                    break;
+
                 }
-                startNewTimer();
             }
-        }
-    };
 
-
-    // таймер до начала/конца пары
-    public void doCicleTimer() {
-        handler.postDelayed(runnable, 250);
-    }
-
-
-    public void startNewTimer() {
-        if (currentDayNum == 5 || currentDayNum == 6) {
-            textViewTimer.setText("Сейчас нет пар");
-            return;
-        }
-
-        cur = new MyTime(
-                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
-                Calendar.getInstance().get(Calendar.MINUTE),
-                Calendar.getInstance().get(Calendar.SECOND)
-        );
-
-        boolean started = false;
-
-        for (int i = 0; i < (10 - calls_type * 2) - 1; i++) {
-            if (MyTime.isBetweenTimes(cur, callsTime[calls_type][i], callsTime[calls_type][i + 1])) {
-                if (i % 2 == 0) {
-                    label = "До конца пары:\n";
-                    curPair = i / 2 + 1;
-                } else {
-                    curPair = 0;
-                    label = "До начала пары:\n";
-                }
-                timeStart = callsTime[calls_type][i];
-                timeEnd = callsTime[calls_type][i + 1];
-                started = true;
-                doCicleTimer();
-                break;
-
+            if (!started) {
+                textViewTimer.setText("Сейчас нет пар");
             }
         }
 
-        if (!started) {
-            textViewTimer.setText("Сейчас нет пар");
+
+        // Обработка нажатий кнопок
+        public void onClick (View view){
+            switch (view.getId()) {
+                case R.id.buttonGroup:
+                    if (group == 1) {
+                        group = 2;
+                    } else {
+                        group = 1;
+                    }
+
+                    buttonGroup.setText(group + "");
+                    currentTab = pager.getCurrentItem();
+                    setTimetable();
+                    break;
+
+                case R.id.buttonToCurrentDate:
+                    updateDateVariables(true);
+                    setTimetable();
+                    buttonToCurrentDate.setEnabled(false);
+                    break;
+
+                case R.id.buttonWeekUp:
+                    calendar.add(Calendar.DAY_OF_YEAR, 7);
+                    updateDateVariables(false);
+                    buttonToCurrentDate.setEnabled(true);
+                    break;
+
+                case R.id.buttonWeekDown:
+                    calendar.add(Calendar.DAY_OF_YEAR, -7);
+                    updateDateVariables(false);
+                    buttonToCurrentDate.setEnabled(true);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
-
-
-    // Обработка нажатий кнопок
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.buttonGroup:
-                if (group == 1) {
-                    group = 2;
-                } else {
-                    group = 1;
-                }
-
-                buttonGroup.setText(group + "");
-                currentTab = pager.getCurrentItem();
-                setTimetable();
-                break;
-
-            case R.id.buttonToCurrentDate:
-                updateDateVariables(true);
-                setTimetable();
-                buttonToCurrentDate.setEnabled(false);
-                break;
-
-            case R.id.buttonWeekUp:
-                calendar.add(Calendar.DAY_OF_YEAR, 7);
-                updateDateVariables(false);
-                buttonToCurrentDate.setEnabled(true);
-                break;
-
-            case R.id.buttonWeekDown:
-                calendar.add(Calendar.DAY_OF_YEAR, -7);
-                updateDateVariables(false);
-                buttonToCurrentDate.setEnabled(true);
-                break;
-
-            default:
-                break;
-        }
-    }
-}
--- app/src/main/java/com/example/timetable1mok/Pair.java
+++ app/src/main/java/com/example/timetable1mok/Pair.java
@@ -9,9 +9,10 @@ public class Pair {
     Integer cabinetNum;     // номер кабинета
     String teacher;         // имя преподавателя
     Integer typePair;       // тип пары (лекция / практика)
+    MyTime timeStart;       // время начала пары
+    MyTime timeEnd;         // время окончания пары
 
-
-    public Pair(Integer dayNum, Integer group, Integer typeWeek, Integer pairNum, String subject, Integer cabinetNum, String teacher, Integer typePair) {
+    public Pair(Integer dayNum, Integer group, Integer typeWeek, Integer pairNum, String subject, Integer cabinetNum, String teacher, Integer typePair, MyTime timeStart, MyTime timeEnd) {
         this.dayNum = dayNum;
         this.group = group;
         this.typeWeek = typeWeek;
@@ -20,6 +21,8 @@ public class Pair {
         this.cabinetNum = cabinetNum;
         this.teacher = teacher;
         this.typePair = typePair;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
     }
 
     public Integer getDayNum() {
@@ -52,5 +55,13 @@ public class Pair {
 
     public Integer getTypePair() {
         return typePair;
+    }
+
+    public MyTime getTimeStart() {
+        return timeStart;
+    }
+
+    public MyTime getTimeEnd() {
+        return timeEnd;
     }
 }
